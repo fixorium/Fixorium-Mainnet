53676f98e8c2b705dbec361e56d874866b91167d
fixorium <fixercoin.token@gmail.com>
2025-07-04 23:58:46 +0500

    Update transaction_sending.py

--- transactions/transaction_sending.py
+++ transactions/transaction_sending.py
@@ -1,1 +1,10 @@
+ 
+class TransactionSending:
+    def __init__(self, sender, recipient, amount):
+        self.sender = sender
+        self.recipient = recipient
+        self.amount = amount
 
+    def send_transaction(self):
+        # Send the transaction
+        print(f'Transaction sent from {self.sender} to {self.recipient} for {self.amount}')
