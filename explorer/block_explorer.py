7db88878c493aff188d7d78ef4c5cd35485f08ca
fixorium <fixercoin.token@gmail.com>
2025-07-04 23:56:38 +0500

    Update block_explorer.py

--- explorer/block_explorer.py
+++ explorer/block_explorer.py
@@ -1,1 +1,11 @@
+ from flask import Flask, request
 
+app = Flask(__name__)
+
+@app.route('/block/<block_hash>', methods=['GET'])
+def get_block(block_hash):
+    # Return the block data
+    return f'Block {block_hash} data'
+
+if __name__ == '__main__':
+    app.run()
