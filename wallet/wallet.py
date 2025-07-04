dbae356994c67f17d3b608074189e347b2fd5eaf
fixorium <fixercoin.token@gmail.com>
2025-07-04 23:57:51 +0500

    Update wallet.py

--- wallet/wallet.py
+++ wallet/wallet.py
@@ -1,1 +1,13 @@
+ import hashlib
 
+class Wallet:
+    def __init__(self):
+        self.private_key = ''
+        self.public_key = ''
+
+    def generate_keys(self):
+        # Generate a new private key
+        self.private_key = hashlib.sha256(str(time.time()).encode()).hexdigest()
+
+        # Generate a public key from the private key
+        self.public_key = hashlib.sha256(self.private_key.encode()).hexdigest()
