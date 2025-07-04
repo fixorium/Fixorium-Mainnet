7ddcb3ab423b583f970ee6e68288342fbab7b5a5
fixorium <fixercoin.token@gmail.com>
2025-07-05 00:02:58 +0500

    Update block.py

--- blockchain/block.py
+++ blockchain/block.py
@@ -1,1 +1,11 @@
+ class Block:
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = ''
 
+    def calculate_hash(self):
+        data_string = str(self.index) + str(self.timestamp) + self.data + self.previous_hash
+        return hashlib.sha256(data_string.encode()).hexdigest()
