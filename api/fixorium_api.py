5a04b45d962e1198a77e57ccd87ca68975b302e9
fixorium <fixercoin.token@gmail.com>
2025-07-04 23:54:47 +0500

    Update fixorium_api.py

--- api/fixorium_api.py
+++ api/fixorium_api.py
@@ -1,1 +1,11 @@
+ from flask import Flask, request
 
+app = Flask(__name__)
+
+@app.route('/blockchain', methods=['GET'])
+def get_blockchain():
+    # Return the blockchain data
+    return 'Blockchain data'
+
+if __name__ == '__main__':
+    app.run()
